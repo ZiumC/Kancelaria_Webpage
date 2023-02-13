542016af8bdaf581cb97c30e9011d71f708890c6
Rafał <90453529+ZiumC@users.noreply.github.com>
2023-02-13 17:42:31 +0100

    Added js for setting and getting value of cookies. Also added js which checks if cookie panel has been hidded on page load

--- Web App/GB_Webpage/GB_Webpage/wwwroot/js/site.js
+++ Web App/GB_Webpage/GB_Webpage/wwwroot/js/site.js
@@ -4,6 +4,7 @@
 // Write your JavaScript code.
 document.getElementById('hide').onclick = function () {
     document.getElementById('cookie-panel').style.display = "none";
+    setCookie("cookie_panel_hidded", "true");
 };
 
 document.getElementById('close-about-cookie').onclick = function () {
@@ -13,3 +14,33 @@ document.getElementById('close-about-cookie').onclick = function () {
 document.getElementById('moreInfo').onclick = function () {
     document.getElementById('about-cookies').style.display = "flex";
 };
+
+window.onload = function () {
+
+    const isCookieHidded = getCookie("cookie_panel_hidded");
+
+    if (isCookieHidded === null || isCookieHidded === undefined || isCookieHidded != "true") {
+        document.getElementById('cookie-panel').style.display = "block";
+    } else {
+        document.getElementById('cookie-panel').style.display = "none";
+    }
+
+};
+
+function getCookie(name) {
+
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+
+    if (parts.length === 2) {
+        return parts
+            .pop()
+            .split(';')
+            .shift();
+    } 
+
+}
+
+function setCookie(name, value) {
+    document.cookie = name + "=" + value + ";";
+}
