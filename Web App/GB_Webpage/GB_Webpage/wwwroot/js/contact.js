5677231c521de22bde6481807779430a899b5c2e
Rafał <90453529+ZiumC@users.noreply.github.com>
2023-02-16 15:10:54 +0100

    Created validation for email

--- Web App/GB_Webpage/GB_Webpage/wwwroot/js/contact.js
+++ Web App/GB_Webpage/GB_Webpage/wwwroot/js/contact.js
@@ -3,12 +3,13 @@
 document.getElementById('send-button').onclick = function () {
 
     let isNameCorrect = validateName();
+    let isEmailCorrect = validateEmail();
 
 
-    if (isNameCorrect) {
+    if (isNameCorrect && isEmailCorrect) {
         $('#contact-form').attr('onsubmit', 'return true;');
     } else {
-       $('#contact-form').attr('onsubmit', 'return false;');
+        $('#contact-form').attr('onsubmit', 'return false;');
     }
 
 };
@@ -20,17 +21,17 @@ function validateName() {
 
     if (name.replace(/\s/g, "") == "" || name == null) {
 
-        $("#error-name").text("To pole jest wymagane.");
-        return false;
-
-    } else if (isNameCorrect == false) {
-
-        $("#error-name").text("To pole jest niepoprawnie uzupełnione.");
+        $("#error-name").text("Pole jest wymagane.");
         return false;
 
     } else if (name.length < 2 || name.length > 50) {
 
         $("#error-name").text("Pole powinno zawierać od 2 do 50 znaków.");
+        return false;
+
+    } else if (isNameCorrect == false) {
+
+        $("#error-name").text("Pole jest niepoprawnie uzupełnione.");
         return false;
 
     } else {
@@ -39,4 +40,33 @@ function validateName() {
         return true;
 
     }
+}
+
+function validateEmail() {
+
+    const email = document.getElementById('input-email').value;
+    const isEmailCorrect = new RegExp("^([\\w\\.\\-]+)@([\\w\\-]+)((\\.(\\w){2,3})+)$").test(email);
+
+    if (email.replace(/\s/g, "") == "" || email == null) {
+
+        $("#error-email").text("Pole jest wymagane.");
+        return false;
+
+    } else if (email.length < 2 || email.length > 50) {
+
+        $("#error-email").text("Pole powinno zawierać od 2 do 50 znaków.");
+        return false;
+
+    } else if (isEmailCorrect == false) {
+
+        $("#error-email").text("E-mail jest niepoprawny.");
+        return false;
+
+    } else {
+
+        $("#error-email").text("");
+        return true;
+
+    }
+
 }
