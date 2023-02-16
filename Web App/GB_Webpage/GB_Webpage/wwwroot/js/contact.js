085c892dc8afd7900ad5ed7d7cff1a9cb3e6e9d6
Rafał <90453529+ZiumC@users.noreply.github.com>
2023-02-16 15:00:32 +0100

    Created validation for name

---
+++ Web App/GB_Webpage/GB_Webpage/wwwroot/js/contact.js
@@ -0,0 +1,44 @@
+﻿
+
+document.getElementById('send-button').onclick = function () {
+
+    let isNameCorrect = validateName();
+
+
+    if (isNameCorrect) {
+        $('#contact-form').attr('onsubmit', 'return true;');
+    } else {
+       $('#contact-form').attr('onsubmit', 'return false;');
+    }
+
+};
+
+function validateName() {
+
+    const name = document.getElementById('input-name').value;
+    const isNameCorrect = new RegExp("^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$").test(name);
+
+
+    //checking Name and Surname field
+    if (name.replace(/\s/g, "") == "" || name == null) {
+
+        $("#error-name").text("To pole jest wymagane.");
+        return false;
+
+    } else if (isNameCorrect == false) {
+
+        $("#error-name").text("To pole jest niepoprawnie uzupełnione.");
+        return false;
+
+    } else if (name.length < 2 || name.length > 50) {
+
+        $("#error-name").text("Pole powinno zawierać od 2 do 50 znaków.");
+        return false;
+
+    } else {
+
+        $("#error-name").text("");
+        return true;
+
+    }
+}
