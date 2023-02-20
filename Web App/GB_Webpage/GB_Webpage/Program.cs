fd5ec194506cf3dce4009ebdfeaf9040dc57cf81
Rafał <90453529+ZiumC@users.noreply.github.com>
2023-02-20 20:57:55 +0100

    Fixed error with title tab, also created mechanism which sets default UI language and remember selected choise

--- Web App/GB_Webpage/GB_Webpage/Program.cs
+++ Web App/GB_Webpage/GB_Webpage/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.AspNetCore.Localization;
+using System.Globalization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,8 +9,9 @@ builder.Services.AddControllersWithViews();
 builder.Services.Configure<RequestLocalizationOptions>(options =>
 {
     options
-    .AddSupportedCultures(new string[] { "pl-PL", "en-US", "de-DE" })
-    .AddSupportedUICultures(new string[] { "pl-PL", "en-US", "de-DE" });
+    .AddSupportedCultures(new string[] { "en-US", "pl-PL", "de-DE" })
+    .AddSupportedUICultures(new string[] { "en-US", "pl-PL", "de-DE" });
+
 });
 
 builder.Services.AddRazorPages();
@@ -18,10 +22,47 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-	app.UseExceptionHandler("/Home/Error");
+    app.UseExceptionHandler("/Home/Error");
 }
 
 app.UseRequestLocalization();
+
+app.Use(async (context, next) =>
+{
+    var contexRequestQuery = context.Request.Query;
+    var contextResponse = context.Response;
+    string cultureQueryRequestString = context.Request.Query["culture"].ToString();
+
+    //if request doesn't have any cookie which specifies what language has been chosen, by default pl-PL is default.
+    if (contexRequestQuery.Count() == 0 || cultureQueryRequestString.Equals(""))
+    {
+
+        Thread.CurrentThread.CurrentUICulture = new CultureInfo("pl-PL");
+
+        contextResponse.Cookies.Append
+        (
+            CookieRequestCultureProvider.DefaultCookieName,
+            CookieRequestCultureProvider.MakeCookieValue(new RequestCulture("pl-PL")),
+            new CookieOptions() { Expires = DateTime.Now.AddYears(1) }
+        );
+
+    }
+    else
+    {
+        Thread.CurrentThread.CurrentUICulture = new CultureInfo(cultureQueryRequestString);
+
+        contextResponse.Cookies.Append
+       (
+           CookieRequestCultureProvider.DefaultCookieName,
+           CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(cultureQueryRequestString)),
+           new CookieOptions() { Expires = DateTime.Now.AddYears(1) }
+       );
+    }
+
+    contextResponse.Redirect(context.Request.Headers["Referer"].ToString());
+
+    await next.Invoke();
+});
 
 app.UseStaticFiles();
 
