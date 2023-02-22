8ddc09367c98c2179e1c289d543925c64ecfd9de
Rafał <90453529+ZiumC@users.noreply.github.com>
2023-02-22 17:06:07 +0100

    Displaying validation errors to form in selected language has been added

--- Web App/GB_Webpage/GB_Webpage/wwwroot/js/contact.js
+++ Web App/GB_Webpage/GB_Webpage/wwwroot/js/contact.js
@@ -27,13 +27,82 @@ setTimeout(() => {
 
 const errorColor = 'red';
 const correctColor = 'lightgray';
+const cookieLangName = '.AspNetCore.Culture';
 const nameRegex = "^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$";
 const emailRegex = "^([\\w\\.\\-]+)@([\\w\\-]+)((\\.(\\w){2,3})+)$";
 const spaceRegex = /\s/g;
 
 
 
+function translateRequired() {
+
+    var languageCookie = getCookie(cookieLangName);
+
+    const pl = "Pole jest wymagane";
+    const en = "Field is required";
+    const de = "tłumaczenie: Pole jest wymagane"
+
+    if (languageCookie.includes("pl")) {
+        return pl;
+    } else if (languageCookie.includes("en")) {
+        return en;
+    } else if (languageCookie.includes("de")) {
+        return de;
+    } else {
+        return pl;
+    }
+
+}
+
+
+
+function translateRange(min, max) {
+
+    var languageCookie = getCookie(cookieLangName);
+
+    const pl = "Pole powinno zawierać " + min + "-" + max + " znaków";
+    const en = "Field should contain " + min + "-" + max + " characters";
+    const de = "tłumaczenie: Pole powinno zawierać " + min + "-" + max + " znaków."
+
+    if (languageCookie.includes("pl")) {
+        return pl;
+    } else if (languageCookie.includes("en")) {
+        return en;
+    } else if (languageCookie.includes("de")) {
+        return de;
+    } else {
+        return pl;
+    }
+}
+
+
+
+function translateFieldProperty() {
+
+    var languageCookie = getCookie(cookieLangName);
+
+    const pl = "Pole jest nieprawidłowo uzupełnione";
+    const en = "Field isn't properly filled";
+    const de = "tłumaczenie: Pole jest nieprawidłowo uzupełnione";
+
+    if (languageCookie.includes("pl")) {
+        return pl;
+    } else if (languageCookie.includes("en")) {
+        return en;
+    } else if (languageCookie.includes("de")) {
+        return de;
+    } else {
+        return pl;
+    }
+
+}
+
+
+
 function validateName() {
+
+    const min = 2;
+    const max = 50;
 
     const element = document.getElementById('input-name');
     const name = element.value;
@@ -43,17 +112,17 @@ function validateName() {
 
     if (name.replace(spaceRegex, "") == "" || name == null) {
 
-        $("#error-name").text("Pole jest wymagane.");
+        $("#error-name").text(translateRequired());
         return false;
 
-    } else if (name.length < 2 || name.length > 50) {
+    } else if (name.length < min || name.length > max) {
 
-        $("#error-name").text("Pole powinno zawierać od 2 do 50 znaków.");
+        $("#error-name").text(translateRange(min, max));
         return false;
 
     } else if (isNameCorrect == false) {
 
-        $("#error-name").text("Pole jest niepoprawnie uzupełnione.");
+        $("#error-name").text(translateFieldProperty());
         return false;
 
     } else {
@@ -69,6 +138,9 @@ function validateName() {
 
 function validateEmail() {
 
+    const min = 2;
+    const max = 50;
+
     const element = document.getElementById('input-email');
     const email = element.value;
     const isEmailCorrect = new RegExp(emailRegex).test(email);
@@ -77,17 +149,17 @@ function validateEmail() {
 
     if (email.replace(spaceRegex, "") == "" || email == null) {
 
-        $("#error-email").text("Pole jest wymagane.");
+        $("#error-email").text(translateRequired());
         return false;
 
-    } else if (email.length < 2 || email.length > 50) {
+    } else if (email.length < min || email.length > max) {
 
-        $("#error-email").text("Pole powinno zawierać od 2 do 50 znaków.");
+        $("#error-email").text(translateRange(min, max));
         return false;
 
     } else if (isEmailCorrect == false) {
 
-        $("#error-email").text("E-mail jest niepoprawny.");
+        $("#error-email").text(translateFieldProperty());
         return false;
 
     } else {
@@ -104,6 +176,9 @@ function validateEmail() {
 
 function validateMessage() {
 
+    const min = 10;
+    const max = 5000;
+
     const element = document.getElementById('input-message');
     const message = element.value;
 
@@ -111,12 +186,12 @@ function validateMessage() {
 
     if (message.replace(spaceRegex, "") == "" || message == null) {
 
-        $("#error-message").text("Pole jest wymagane.");
+        $("#error-message").text(translateRequired());
         return false;
 
-    } else if (message.length < 10 || message.length > 5000) {
+    } else if (message.length < min || message.length > max) {
 
-        $("#error-message").text("Pole powinno zawierać od 10 do 5000 znaków.");
+        $("#error-message").text(translateRange(min, max));
         return false;
 
     } else {
