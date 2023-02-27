563b7fff6cb3ac54d2e432be9658d976965f47f2
Rafał <90453529+ZiumC@users.noreply.github.com>
2023-02-27 21:29:27 +0100

    Added theme handling and sorting handling for sort button

---
+++ Web App/GB_Webpage/GB_Webpage/wwwroot/js/news.js
@@ -0,0 +1,23 @@
+﻿document.getElementById('sort-button').onclick = function () {
+
+    var sortArrow = document.getElementById('sort-arrow');
+
+    const currentClass = String(sortArrow.classList).split('-');
+    const currentThemeClass = currentClass[0] + "-" + currentClass[1];
+    const currentSort = currentClass[2];
+
+    
+    if (currentSort == "asc") {
+
+        sortArrow.classList = currentThemeClass + '-desc';
+
+    } else {
+
+        sortArrow.classList = currentThemeClass + '-asc';
+
+    }
+
+}
+
+
+
