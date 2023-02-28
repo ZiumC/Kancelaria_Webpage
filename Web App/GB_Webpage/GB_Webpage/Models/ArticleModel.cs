8049f8b65b04694576db70377b4ef293cedb49c3
Rafał <90453529+ZiumC@users.noreply.github.com>
2023-02-28 13:22:02 +0100

    initial commit for this branch

---
+++ Web App/GB_Webpage/GB_Webpage/Models/ArticleModel.cs
@@ -0,0 +1,10 @@
+﻿namespace GB_Webpage.Models
+{
+    public class ArticleModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
