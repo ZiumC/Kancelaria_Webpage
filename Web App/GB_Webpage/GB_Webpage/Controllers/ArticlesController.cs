25d6fa7157c0e2d8e987c9ff879ad8a20c0e68e0
Rafał <90453529+ZiumC@users.noreply.github.com>
2023-02-28 16:01:33 +0100

    REST API has been created then added then prepared to proper work

---
+++ Web App/GB_Webpage/GB_Webpage/Controllers/ArticlesController.cs
@@ -0,0 +1,29 @@
+﻿using GB_Webpage.Data;
+using GB_Webpage.Models;
+using GB_Webpage.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GB_Webpage.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ArticlesController : ControllerBase
+    {
+
+        private readonly IApiService _apiService;
+
+        public ArticlesController(IApiService apiService)
+        {
+            _apiService = apiService;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllArticles()
+        {
+            ArticleModel model = await _apiService.GetAllArticles();
+
+            return Ok(model);
+        }
+    }
+}
---
+++ Web App/GB_Webpage/GB_Webpage/Services/IApiService.cs
@@ -0,0 +1,9 @@
+﻿using GB_Webpage.Models;
+
+namespace GB_Webpage.Services
+{
+    public interface IApiService
+    {
+        public Task<ArticleModel> GetAllArticles();
+    }
+}
