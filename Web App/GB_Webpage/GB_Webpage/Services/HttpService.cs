fd8cf4ce7e19c8ad1438e764c27e3646455c7c1d
Rafał <90453529+ZiumC@users.noreply.github.com>
2023-02-28 18:01:25 +0100

    Created separated class for http get requests

---
+++ Web App/GB_Webpage/GB_Webpage/Services/HttpService.cs
@@ -0,0 +1,34 @@
+﻿using GB_Webpage.Models;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+
+namespace GB_Webpage.Services
+{
+    public class HttpService
+    {
+
+        public static async Task<T?> MakeGetRequest<T>(string url)
+        {
+
+            using (var client = new HttpClient())
+            {
+                Uri uri = new Uri(url);
+
+                HttpResponseMessage response = await client.GetAsync(uri);
+
+                string responseResult = await response.Content.ReadAsStringAsync();
+
+                if (responseResult != null)
+                {
+                    return JsonConvert.DeserializeObject<T>(responseResult);
+                }
+                else
+                {
+                    return default;
+                }
+
+            }
+
+        }
+    }
+}
