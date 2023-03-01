09e2108d0dd9c55771f6158c9fb3290cc8b981de
Rafał <90453529+ZiumC@users.noreply.github.com>
2023-03-01 09:57:35 +0100

    Creaded service for saving data db in physical file

---
+++ Web App/GB_Webpage/GB_Webpage/Services/DatabaseFileService.cs
@@ -0,0 +1,49 @@
+﻿using GB_Webpage.Models;
+using Newtonsoft.Json;
+
+namespace GB_Webpage.Services
+{
+    public class DatabaseFileService
+    {
+
+        private readonly string _path;
+
+        public DatabaseFileService()
+        {
+            _path = "";
+        }
+
+        public bool SaveFile<T>(T data)
+        {
+
+            string jsonData = JsonConvert.SerializeObject(data);
+
+            if (File.Exists(_path))
+            {
+                File.Delete(_path);
+            }
+
+            TextWriter writer = null;
+            try
+            {
+                writer = new StreamWriter(_path, false);
+                writer.Write(jsonData);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+            finally
+            {
+                if (writer != null)
+                {
+                    writer.Close();
+                }
+            }
+
+            return true;
+        }
+
+    }
+}
