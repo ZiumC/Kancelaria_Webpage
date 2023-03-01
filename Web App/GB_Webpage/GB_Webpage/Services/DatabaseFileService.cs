e1cbe8b67674a795981c3b8eee82b26be0879b8b
Rafał <90453529+ZiumC@users.noreply.github.com>
2023-03-01 11:22:24 +0100

    Added method wich can read files

--- Web App/GB_Webpage/GB_Webpage/Services/DatabaseFileService.cs
+++ Web App/GB_Webpage/GB_Webpage/Services/DatabaseFileService.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json;
+﻿using GB_Webpage.Models;
+using Newtonsoft.Json;
 
 namespace GB_Webpage.Services
 {
@@ -65,5 +66,36 @@ namespace GB_Webpage.Services
             return true;
         }
 
+        public T? ReadFile<T>()
+        {
+
+            string path = GetDirFiles();
+
+            if (Directory.Exists(path))
+            {
+                return default;
+            }
+
+            TextReader reader = null;
+            try
+            {
+                reader = new StreamReader(path);
+                var fileContents = reader.ReadToEnd();
+                return JsonConvert.DeserializeObject<T>(fileContents);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return default;
+            }
+            finally
+            {
+                if (reader != null)
+                {
+                    reader.Close();
+                }
+            }
+
+        }
     }
 }
