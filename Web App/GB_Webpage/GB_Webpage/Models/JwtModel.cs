9c611d4e0544811a9c6f8d22709b9b8bc6da5543
Rafał <90453529+ZiumC@users.noreply.github.com>
2023-03-03 15:00:33 +0100

    Added JwtModel for saving refreshToken into physical file. Also refreshToken is saved every time when user login.

---
+++ Web App/GB_Webpage/GB_Webpage/Models/JwtModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GB_Webpage.Models
+{
+    public class JwtModel
+    {
+        [Required]
+        public string AccessToken{ get; set; }
+        [Required]
+        public string RefreshToken{ get; set; }
+    }
+}
