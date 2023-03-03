4766c797b5bcf24eff05b496c7feae45e542ee70
Rafał <90453529+ZiumC@users.noreply.github.com>
2023-03-03 13:16:41 +0100

    Added login in endpoint, working on jwt

---
+++ Web App/GB_Webpage/GB_Webpage/Services/UserService.cs
@@ -0,0 +1,19 @@
+﻿using GB_Webpage.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace GB_Webpage.Services
+{
+    public class UserService
+    {
+        public static bool VerifyUserPassword(LoginRequest currentUser, string passedPassword, string salt)
+        {
+            var hasher = new PasswordHasher<LoginRequest>();
+
+            var hashedCurrentPassword = hasher.HashPassword(currentUser, currentUser.Password + salt);
+
+            PasswordVerificationResult result = hasher.VerifyHashedPassword(currentUser, hashedCurrentPassword, passedPassword + salt);
+
+            return result.ToString().Equals("Success");
+        }
+    }
+}
