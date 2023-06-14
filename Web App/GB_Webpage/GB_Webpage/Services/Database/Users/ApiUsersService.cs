a7b3bdbdc79dfe5e7d2d52cb8e16e299243788a6
Rafał <90453529+ZiumC@users.noreply.github.com>
2023-06-14 18:55:33 +0200

    Added methods to ApiUsersService

--- Web App/GB_Webpage/GB_Webpage/Services/Database/Users/ApiUsersService.cs
+++ Web App/GB_Webpage/GB_Webpage/Services/Database/Users/ApiUsersService.cs
@@ -1,19 +1,30 @@
-﻿namespace GB_Webpage.Services.Database.Users
+﻿using GB_Webpage.Data;
+
+namespace GB_Webpage.Services.Database.Users
 {
     public class ApiUsersService : IApiUsersService
     {
+        private readonly ApiContext _context;
+        private readonly ILogger<ApiUsersService> _logger;
 
-        public Task<bool> AddUserToBlocklistAsync(string userName, int attempCount, DateTime dateFrom)
+        public ApiUsersService(ApiContext context, ILogger<ApiUsersService> logger)
+        {
+            _context = context;
+            _context.Database.EnsureCreated();
+            _logger = logger;
+        }
+
+        public async Task<bool> AddUserToBlocklistAsync(string userName, int attempCount, DateTime dateFrom)
         {
             throw new NotImplementedException();
         }
 
-        public Task<bool> GetUserFataFromBlacklistAsync(string userName)
+        public async Task<bool> GetUserFataFromBlacklistAsync(string userName)
         {
             throw new NotImplementedException();
         }
 
-        public Task<bool> UpdateUserInBlacklistAsync(string userName, int attempCount, DateTime dateFrom, DateTime dateTo)
+        public async Task<bool> UpdateUserInBlacklistAsync(string userName, int attempCount, DateTime dateFrom, DateTime dateTo)
         {
             throw new NotImplementedException();
         }
