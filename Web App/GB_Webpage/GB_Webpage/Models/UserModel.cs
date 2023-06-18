5f08a505c2e5f7ea0d4eadfef80a22f9b3dfd03b
Rafał <90453529+ZiumC@users.noreply.github.com>
2023-06-18 19:01:24 +0200

    Working on login method but it has an bug with checking date

---
+++ Web App/GB_Webpage/GB_Webpage/Models/UserModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GB_Webpage.Models
+{
+    public class UserModel
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}
--- Web App/GB_Webpage/GB_Webpage/Services/User/IUserService.cs
+++ Web App/GB_Webpage/GB_Webpage/Services/User/IUserService.cs
@@ -4,10 +4,9 @@ namespace GB_Webpage.Services.User
 {
     public interface IUserService
     {
-        public bool VerifyPassword(LoginRequestDTO currentUser, string password);
         public string GenerateRefreshToken();
         public string GenerateAccessToken(string user);
         public bool ValidateTokens(JwtDTO jwt);
-
+        public bool isUserValidFor(LoginRequestDTO loginRequest);
     }
 }
