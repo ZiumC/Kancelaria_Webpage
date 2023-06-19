3fe46fdb7b4607e07a45c971eac57e08834260b0
Rafał <90453529+ZiumC@users.noreply.github.com>
2023-06-19 13:46:04 +0200

    Suspending feature works great

--- Web App/GB_Webpage/GB_Webpage/Services/Database/ApiUsers/IApiUsersService.cs
+++ Web App/GB_Webpage/GB_Webpage/Services/Database/ApiUsers/IApiUsersService.cs
@@ -4,6 +4,7 @@ namespace GB_Webpage.Services.Database.Users
 {
     public interface IApiUsersService
     {
+        public Task<IEnumerable<SuspendedUserModel>> GetAllSuspendedUsers();
         public Task<SuspendedUserModel?> GetUserDataFromBlacklistAsync(string userName);
         public Task<bool> AddUserToBlocklistAsync(string userName);
         public Task<bool> UpdateUserInBlockedlistAsync(int blockedUserId, SuspendedUserModel blockedUserData);
