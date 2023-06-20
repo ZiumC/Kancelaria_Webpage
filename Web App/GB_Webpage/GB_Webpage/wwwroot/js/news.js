4defa0d824dc73f4337908631fbaf9a1f37d0b0e
Rafał <90453529+ZiumC@users.noreply.github.com>
2023-06-20 12:08:14 +0200

    Working on paging table in news page

--- Web App/GB_Webpage/GB_Webpage/wwwroot/js/news.js
+++ Web App/GB_Webpage/GB_Webpage/wwwroot/js/news.js
@@ -47,8 +47,6 @@ function sortElementsBy(type) {
             return convertDate(dateSecond) - convertDate(dateFirst);
 
         }
-
-
     });
 
     rows.forEach(function (element) {
@@ -64,4 +62,29 @@ function convertDate(d) {
 }
 
 
+$(document).ready(function () {
+    $('#news').after('<div id="paging-nav"></div>');
+    var rowsShown = 10;
+    var rowsTotal = $('#news tbody tr').length;
+    var numPages = rowsTotal / rowsShown;
+    for (i = 0; i < numPages; i++) {
+        var pageNum = i + 1;
+        $('#paging-nav').append('<a class="btn btn-secondary" href="#" rel="' + i + '">' + pageNum + '</a> ');
+    }
+    $('#data tbody tr').hide();
+    $('#data tbody tr').slice(0, rowsShown).show();
+    $('#paging-nav a:first').addClass('active btn-primary');
+    $('#paging-nav a:first').removeClass('btn-secondary');
+    $('#paging-nav a').bind('click', function () {
+        $('#paging-nav a').addClass('btn-secondary');
+        $('#paging-nav a').removeClass('active btn-primary');
+        $(this).addClass('active btn-primary');
+        $(this).removeClass('btn-secondary');
 
+        var currPage = $(this).attr('rel');
+        var startItem = currPage * rowsShown;
+        var endItem = startItem + rowsShown;
+        $('#news tbody tr').css('opacity', '0.0').hide().slice(startItem, endItem).
+            css('display', 'table-row').animate({ opacity: 1 }, 300);
+    });
+});
